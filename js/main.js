d12844ea8980a1ded0422f99d41cee8ca9d8d148
CoderDi <lvbnhbq2103@mail.ru>
2021-01-16 23:35:01 +0700

    ready

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
   $(".js-module-open").on("click", function(){
-    $('.' + $(this).data("module")).slideDown(500);
+    if ($(this).data("module") == "module2") {
+      $(".module__open3").css("padding-top", 70);
+      $(".module3-img").css("top", 70);
+    } else {
+      $(".module__open3").addClass("open");
+      $(".module3").css("padding-top", 250);
+      $(".modules").css("padding-bottom", 0);
+    }
+    
+    $('.' + $(this).data("module")).show();
   });
   
 });
